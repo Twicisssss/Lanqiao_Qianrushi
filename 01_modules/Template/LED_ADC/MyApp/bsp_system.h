e742b3e39500ce185bc7214bfa8a2af1bb2d516c
Twicis <dusk12521@outlook.com>
2026-04-08 11:26:36 +0800

    测试

---
+++ 01_modules/Template/LED_ADC/MyApp/bsp_system.h
@@ -0,0 +1,18 @@
+#ifndef __BSP_SYSTEM_H_
+#define __BSP_SYSTEM_H_
+
+#include "main.h"
+#include "usart.h"
+
+#include "system.h"
+#include "scheduler.h"
+
+#include "led_app.h"
+extern uint8_t led_buf[8];
+#include "key_app.h"
+#include "lcd_app.h"
+#include "uart_app.h"
+
+
+#endif
+
---
+++ 01_modules/Template/LED_ADC/MyApp/led_app.c
@@ -0,0 +1,28 @@
+#include "led_app.h"
+uint8_t led_buf[8]={0};
+void led_disp(uint8_t *uc_led)
+{
+    uint8_t temp_now = 0x00;
+    uint8_t temp_old = 0xff;
+
+    for(uint8_t i=0;i<8;i++)
+        temp_now |=(uc_led[i] <<i);
+
+
+    if(temp_now!=temp_old)
+    {
+        GPIOC->ODR &= 0x00ff;
+        GPIOC->ODR |= ~temp_now<<8;
+        GPIOD->BSRR |= 0x01<<2;
+        GPIOD->BSR |= 0x01<<2;
+
+        temp_old=temp_now;
+    }
+}
+
+void led_task(void)
+{
+    led_buf[0]=1;
+    led_disp(led_buf);
+}
+
